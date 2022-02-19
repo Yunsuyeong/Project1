5dbb15683a2fb13798315cb83ce53c2c1b4c326a
Yunsuyeong <98012833+Yunsuyeong@users.noreply.github.com>
2022-02-20 00:32:50 +0900

    ver 1.1

---
+++ JS/background.js
@@ -0,0 +1,11 @@
+const backgrounds=[
+    "background1.jpg", "background2.jpg"
+];
+
+const currentImage=backgrounds[Math.floor(Math.random()*backgrounds.length)];
+
+const background=document.createElement("img");
+
+background.src=`${currentImage}`;
+
+document.body.appendChild(background);
